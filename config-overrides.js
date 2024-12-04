2dfbad8f03c0e18d7b341ece916e681a70b38fbd
macshire <maxmacshire@gmail.com>
2024-12-04 16:53:21 +0700

    config override file fixed most errors
    need to fix process/browser error

---
+++ config-overrides.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = function override(config) {
+  // Add polyfills for Node modules in the browser
+  config.resolve.fallback = {
+    ...config.resolve.fallback, // Retain existing fallbacks
+    fs: false, // File system is not supported in browsers
+    net: false, // Networking modules are not supported
+    zlib: require.resolve("browserify-zlib"),
+    querystring: require.resolve("querystring-es3"),
+    path: require.resolve("path-browserify"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    http: require.resolve("stream-http"),
+    process: require.resolve("process/browser"),
+  };
+
+  // Add plugins for polyfills
+  config.plugins = [
+    ...(config.plugins || []),
+    new webpack.ProvidePlugin({
+      process: 'process/browser', // Make `process` available globally
+    }),
+    new NodePolyfillPlugin(), // Add other Node.js polyfills
+  ];
+
+  return config;
+};
